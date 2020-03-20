8efe98f8f77bd4a0656b984c346fabd189371f1e
Yuri <yuriand1991@outlook.com>
2020-03-20 20:33:57 +0300

    test add order

--- app/Http/Controllers/OrderController.php
+++ app/Http/Controllers/OrderController.php
@@ -4,12 +4,20 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Http\Requests\OrderRequest;
+use App\Order;
 
 class OrderController extends Controller
 {
     //
 	public function submit(OrderRequest $req)
 	{
+		$order = new Order();
+		$order->status = $req->input('status');
+		$order->client_email = $req->input('email');
+		$order->partner_id = $req->input('partner');
+		$order->save();
+		
+		return redirect()->route('home')->with('success', 'Заказ добавлен');
 		//$validation = $req->validate([
 									//'email' => 'required|email'
 								//	]);
